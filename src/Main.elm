43efdf22fd0ad56cf2f8fd7dfefcc2965f31daec
Chiharu Terashima <chichi.pa2.1091@gmail.com>
2020-09-09 22:01:15 +0900

    format

--- src/Main.elm
+++ src/Main.elm
@@ -1,72 +1,83 @@
 -- Main.elm
+
+
 module Main exposing (main)
 
 import Browser
-import Html exposing (Html, text, div, button, table)
-import Html exposing (tr, th, td, thead, tbody, thead)
+import Html exposing (Html, button, div, table, tbody, td, text, th, thead, tr)
 import Html.Events exposing (onClick)
 
+
 main =
-  Browser.sandbox { init = init, update = update, view = view }
+    Browser.sandbox { init = init, update = update, view = view }
+
 
 
 -- MODEL
 
+
 type alias Task =
-  {
-    id: Int,
-    task: String
-  }
+    { id : Int
+    , task : String
+    }
 
-type alias Model = 
-  { todoList : List Task }
 
-init: Model
+type alias Model =
+    { todoList : List Task }
+
+
+init : Model
 init =
-  {
-    todoList = [
-      { id = 1, task = "task1"}
-      ,{ id = 2, task = "task2"}
-    ]
-  }
+    { todoList =
+        [ { id = 1, task = "task1" }
+        , { id = 2, task = "task2" }
+        ]
+    }
+
+
 
 -- UPDATE
 
-type Msg = Add
+
+type Msg
+    = Add
+
 
 update : Msg -> Model -> Model
 update msg model =
-  case msg of
-    Add ->
-      { model | todoList = {id = 3, task = "task3"} :: model.todoList}
-    
+    case msg of
+        Add ->
+            { model | todoList = { id = 3, task = "task3" } :: model.todoList }
+
+
 
 -- VIEW
 
+
 view : Model -> Html Msg
 view model =
-  div []
-    [
-      div []
-        [ table []
-          [ thead []
-            [ tr []
-              [ th [] [text "ID"]
-              , th [] [text "TASK"]
-              ]
+    div []
+        [ div []
+            [ table []
+                [ thead []
+                    [ tr []
+                        [ th [] [ text "ID" ]
+                        , th [] [ text "TASK" ]
+                        ]
+                    ]
+                ]
+            , tbody [] (todoRow model.todoList)
             ]
-          ]
-          , tbody [] (todoRow model.todoList)
         ]
-    ]
+
 
 todoRow : List Task -> List (Html Msg)
 todoRow todos =
-  let
-    show (todo) = tr []
-      [ td [] [text (String.fromInt todo.id)]
-      , td [] [text todo.task]
-      ]
-
-  in
+    let
+        show todo =
+            tr []
+                [ td [] [ text (String.fromInt todo.id) ]
+                , td [] [ text todo.task ]
+                ]
+    in
     List.map show todos
